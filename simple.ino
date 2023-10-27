a7210bd6318dd8c6abbbb3960984cef9093fa4fb
BHG <130232699+For-symposium@users.noreply.github.com>
2023-10-27 16:08:47 +0900

    Update simple.ino

--- simple.ino
+++ simple.ino
@@ -1,3 +1,5 @@
+// 방향 조향 code
+
 // 모터 제어를 위한 핀 설정
 int motorDirection1Pin = 8; // 모터 방향 핀 1
 int motorDirection2Pin = 9; // 모터 방향 핀 2
@@ -19,28 +21,24 @@ void setup() {
 
 void loop() {
   //int val=analogRead(analogPin);
-  //int motorSpeed=map(val, 100, 130, ToLow, ToHigh);
-  //analogWrite(motorDirection1Pin, motorSpeed);
-  //analogWrite(motorDirection2Pin, motorSpeed);
-  //motor_forward(motorA1, motorA2, 75);
-  //motor_forward(motorA3, motorA4, 75);
+  // int motorSpeed=map(val, 100, 130, ToLow, ToHigh);
+  analogWrite(motorDirection1Pin, 100);
+  analogWrite(motorDirection2Pin, 100);
+  
   
   // 모터를 좌측으로 회전하도록 설정
-  digitalWrite(motorDirection1Pin, LOW); // 모터 방향 핀 1 설정 (전진)
-  digitalWrite(motorDirection2Pin, HIGH); // 모터 방향 핀 2 설정 (반대 방향)
+  // digitalWrite(motorDirection1Pin, LOW); // 모터 방향 핀 1 설정 (전진)
+  // digitalWrite(motorDirection2Pin, HIGH); // 모터 방향 핀 2 설정 (반대 방향)
   
    // 일정 시간 동안 회전
   delay(2000); // 2초 동안 회전
+
+  analogWrite(motorDirection1Pin, 10);
+  analogWrite(motorDirection2Pin, 10);
   
-  digitalWrite(motorDirection1Pin, HIGH); // 모터 방향 핀 1 설정 (전진)
-  digitalWrite(motorDirection2Pin, LOW); // 모터 방향 핀 2 설정 (반대 방향)
+  // digitalWrite(motorDirection1Pin, HIGH); // 모터 방향 핀 1 설정 (전진)
+  // digitalWrite(motorDirection2Pin, LOW); // 모터 방향 핀 2 설정 (반대 방향)
 
   // 일정 시간 동안 회전
   delay(2000); // 2초 동안 회전
-  
- 
-  
-  
-  // 일정 시간 동안 정지
-  delay(2000); // 2초 동안 정지
 }
